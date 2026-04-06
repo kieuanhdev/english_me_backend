14873c50514d53bc721ee3b2be248e54b876b60d
kieuanhdev <kieuanh.dev@gmail.com>
2026-04-06 15:33:01 +0700

    create project english me

---
+++ src/main/java/vn/id/kieuanhdev/englishme/EnglishmeApplication.java
@@ -0,0 +1,13 @@
+package vn.id.kieuanhdev.englishme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/id/kieuanhdev/englishme/EnglishmeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.id.kieuanhdev.englishme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
