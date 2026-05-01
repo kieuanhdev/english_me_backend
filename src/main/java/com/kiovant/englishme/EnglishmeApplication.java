37d460fb2481b9f0c577f5cf6e16f64cc4ae9812
kieuanhdev <kieuanh.dev@gmail.com>
2026-05-01 20:16:47 +0700

    init project

---
+++ src/main/java/com/kiovant/englishme/EnglishmeApplication.java
@@ -0,0 +1,13 @@
+package com.kiovant.englishme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiovant/englishme/EnglishmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiovant.englishme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
